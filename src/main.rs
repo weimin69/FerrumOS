0aaad897a445614f764fd3a4f12c37bb2eda4108
WeiminKng <weiminkong2@gmail.com>
2026-09-15 22:58:32 +0800

    Add bare-metal compilation target“

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ use core::panic::PanicInfo;
 
 #[unsafe(no_mangle)]//使用c语言的调用约定
 pub extern "C" fn _start() -> ! {
+    //此处默认命名为_start
     loop {}
 }
 
 #[panic_handler]//panic时调用
-fn panic(_info:&PanicInfo) {
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
